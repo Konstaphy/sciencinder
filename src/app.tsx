a2f9fe877d7f4a523f2d0e4ed457a262728b80a0
Konstantine <Konstaphy@gmail.com>
2022-10-08 16:36:15 +0500

    feat: добавил реакт в проект

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+export function App() {
+  return (
+    <div className="app">
+      <header className="App-header">
+
+      </header>
+    </div>
+  );
+}
