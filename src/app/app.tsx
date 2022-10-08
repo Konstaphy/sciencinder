5ea522a070f0a416749d73947afc91c6dcb99c2c
Konstantine <Konstaphy@gmail.com>
2022-10-08 16:46:44 +0500

    feat: добавил линтер

---
+++ src/app/app.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+
+export function App() {
+  return (
+    <div className="app">
+      <header className="App-header"></header>
+    </div>
+  );
+}
