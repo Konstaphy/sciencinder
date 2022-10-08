afa1249c09585cafc8ce9f403b6707f6455f1099
Konstantine <Konstaphy@gmail.com>
2022-10-08 21:35:35 +0500

    feat: сделал форму логина

--- src/pages/login-page/login-page.tsx
+++ src/pages/login-page/login-page.tsx
@@ -1,10 +1,22 @@
 import "./login-page.css";
-import { Section } from "../../shared/components/section/section";
+import { FullscreenSection } from "../../shared/ui/fullscreen-section/fullscreen-section";
+import { Input } from "../../shared/ui/input/input";
 
 export const LoginPage = () => {
   return (
     <div className="login-page">
-      <Section>123</Section>
+      <FullscreenSection buttonText="Войти" onClick={() => alert()}>
+        <Input title="Имя" placeholder="Введите ваше имя" />
+        <Input title="Фамилия" placeholder="Введите вашу фамилию" />
+        <Input
+          title="Академическая группа"
+          placeholder="Введите вашу академ группу, например: РИ-101010"
+        />
+        <Input
+          title="Академическая группа"
+          placeholder="Введите вашу академ группу, например: РИ-101010"
+        />
+      </FullscreenSection>
     </div>
   );
 };
